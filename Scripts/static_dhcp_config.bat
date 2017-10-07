932671c719b0f83fb565a28b1635e14c26bf21bb
Glenn Lopez <glennlopez@gmail.com>
2017-10-07 09:49:12 -0500

    added clear screen action

--- Scripts/static_dhcp_config.bat
+++ Scripts/static_dhcp_config.bat
@@ -15,18 +15,22 @@ for %%? in (B) do if /I "%C%"=="%%?" goto B
 goto choice 
 :A 
 @echo off 
+cls
 echo [!] Please enter Static IP Address Information
 echo Static IP Address: 
 set /p IP_Addr=
 
+cls
 echo [!] WARNING: Script doesnt work if Gateway is left blank 
 echo Default Gateway:
 set /p D_Gate=
 
+cls
 echo [!] WARNING: Script doesnt work if Subnet Mask is left blank
 echo Subnet Mask: 
 set /p Sub_Mask=
 
+cls
 echo Setting Static IP Information 
 netsh interface ip set address "mcse" static %IP_Addr% %Sub_Mask% %D_Gate% 1 
 netsh int ip show config 
