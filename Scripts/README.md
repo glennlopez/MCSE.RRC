e57236b5897a098e3ef2619c48fbc6c0a831c458
Glenn Lopez <glennlopez@gmail.com>
2017-10-08 22:55:38 -0500

    Added no gateway script

--- Scripts/README.md
+++ Scripts/README.md
@@ -1,3 +1,10 @@
 Scripts
 ---
 These scripts were made specifically for removing the "busy-work" out of lab configurations in class. Do not use these if you dont know how to manually configure the labs yourself.
+
+
+Troubleshooting/Known Bugs
+---
+* Network Config scripts don't seem to work on Server 2008 R2
+* Script doesnt change any of the network config:
+ > Fix this by renaming your network adaptor from "Local Area Connection" to "mcse" (case-sensitive)
---
+++ Scripts/static_dhcp_config_noGateWay.bat
@@ -0,0 +1,46 @@
+@echo off 
+echo.
+REM updated 10/5/2017 - Glenn
+echo Glenns Network Config-er: NO GATEWAY
+echo (because using the mouse to configure your network is a waste of time)
+echo.
+echo [A] Set Static IP 
+echo [B] Set DHCP 
+echo. 
+
+:choice 
+SET /P C=Choose A or B: 
+for %%? in (A) do if /I "%C%"=="%%?" goto A 
+for %%? in (B) do if /I "%C%"=="%%?" goto B 
+goto choice 
+:A 
+@echo off 
+cls
+echo [!] Please enter Static IP Address Information
+echo Static IP Address: 
+set /p IP_Addr=
+echo [!] WARNING: Script doesnt work if Subnet Mask is left blank
+echo Subnet Mask: 
+set /p Sub_Mask=
+
+cls
+echo Setting Static IP Information 
+netsh interface ip set address "mcse" static %IP_Addr% %Sub_Mask% 1 
+netsh int ip show config 
+pause 
+goto end
+
+:B 
+@ECHO OFF 
+ECHO Resetting IP Address and Subnet Mask For DHCP 
+netsh int ip set address name = "mcse" source = dhcp
+
+ipconfig /renew
+
+ECHO Here are the new settings for %computername%: 
+netsh int ip show config
+
+pause 
+goto end 
+:end
+
