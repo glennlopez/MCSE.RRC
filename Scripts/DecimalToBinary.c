cd5b0853e4c09d58616508cf7ac15baed122af85
Glenn Lopez <glennlopez@gmail.com>
2017-10-18 15:22:48 -0500

    added spacer

--- Scripts/DecimalToBinary.c
+++ Scripts/DecimalToBinary.c
@@ -22,8 +22,14 @@ int main () {
     convert(usrInt, host);
 
     //print output to screen
-    for(int i = 32; i >= 0; i--){
+    for(int i = 31; i >= 0; i--){
         printf("%i", host[i]);
+
+        //octet spacer
+        if(!(i % 8)){
+            printf(" ");
+        }
+
     }
     printf("\n");
 
