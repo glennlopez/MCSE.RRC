258860a33750472bfed13bfa80503aecbb230ba0
Glenn Lopez <glennlopez@gmail.com>
2017-10-20 13:02:37 -0500

    added AND calculator subroutine

--- Scripts/DecimalToBinary.c
+++ Scripts/DecimalToBinary.c
@@ -8,23 +8,22 @@
 #define NIBBLE 4
 
 //SUBROUTINE PROTOTYPES 
-void convert(int, int*);    //convert(decimal to binary, stored at an array[])
-void printArray(int*);      //print(contents of array[])
-void calcNetwork(int*);     //calculateNetworkAddress(store the result at an array[])
+void convertBin(int, int*);         //convert(decimal to binary, stored at an array[])
+int convertDec(int*);               //convers binary from an array[] and returns decimal
+void printArray(int*);              //print(contents of array[])
+void calcAND(int*, int*, int*);     //Logic AND calculator func(binary, binary, store results in array[])
 
 //GLOBAL VARIABLES
     //FIXME: use 2 dimentional arrays for storing IP numbers
 int host[DWORD];                       //stores user input in binary
-int subnet[DWORD] = {1,1,1,0,1};       //stores user subnet mask in binary using cidr notation
+int subnet[DWORD] = {0,1,1,1};       //stores user subnet mask in binary using cidr notation
 int network[DWORD];                    //stores result of host AND-ed subnet
 
 
 //MAIN ROUTINE
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) { char usrStr[10];
 
-    char usrStr[10];
-
-    //Allow user input via command-line argument 
+    //Command-line argument 
     if(argc != 2){
         printf("Decimal to convert: ");
         scanf("%s", usrStr);
@@ -34,12 +33,19 @@ int main(int argc, char* argv[]) {
             usrStr[i] = argv[1][i];
         }
     }
-
     int usrInt = atoi(usrStr);
 
-    convert(usrInt, host);
+    //DEBUG - print outs
+    convertBin(usrInt, host);
+    printf("A: ");
     printArray(host);
+
+    printf("B: ");
     printArray(subnet);
+
+    calcAND(host, subnet, network);
+
+    printf("Z: ");
     printArray(network);
 
     return(0);
@@ -48,9 +54,15 @@ int main(int argc, char* argv[]) {
 
 
 //NETWORK ADDRESS CALCULATION SUBROUTINE
-void calcNetwork(int* param){
+void calcAND(int* param1, int* param2, int* result){
     //FIXME: make this happen
+    int BITLIMIT = OCTET;
+    
+    for(int i = 0; i < BITLIMIT; i++){
+        result[i] = param1[i] & param2[i];
+    }
 }
+
 
 
 //PRINT-OUT SUBROUTINE
@@ -71,7 +83,7 @@ void printArray(int* param){
 
 
 //DECIMAL TO BINARY SUBROUTINE
-void convert(int param, int* param2){ int count = 0;
+void convertBin(int param, int* param2){ int count = 0;
 
     //Divide n by 2 and store remainder as a binary 1 until n = 1
     if(param == 1){
@@ -94,4 +106,12 @@ void convert(int param, int* param2){ int count = 0;
 
         count++;
     }
+}
+
+
+
+//BINARY TO DECIMAL SUBROUTINE
+int convertDec(int* param){
+    //FIXME: make this happen
+    return 0;
 }
